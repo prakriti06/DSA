4a707b727f20ca01ca04cbf50d304c1c35f5b212
Prakriti Sharma <prakritisharma957@gmail.com>
2022-01-02 20:57:31 +0530

    Time: 12 ms (9.34%), Space: 11.7 MB (12.07%) - LeetHub

--- find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
+++ find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
@@ -1,11 +1,49 @@
 class Solution {
 public:
+    
+    int LB(vector<int>& arr, int x){
+    int l=0, r=arr.size()-1, mid, ans=-1;
+    while(l<=r){
+        mid= l+(r-l)/2;
+        if(arr[mid]==x){
+            r=mid-1;
+            ans= mid;
+        }
+        else if(arr[mid]>x)
+            r=mid-1;
+        else{
+            l=mid+1;
+        }
+    }
+    return ans;
+}
+    
+    int UB(vector<int>& arr, int x){
+    int l=0, r=arr.size()-1, mid, ans=-1;
+    while(l<=r){
+        mid= l+(r-l)/2;
+        if(arr[mid]==x){
+            l=mid+1;
+            ans=mid;
+        }
+        else if(arr[mid]<x)
+            l=mid+1;
+        else{
+            r=mid-1;
+        }
+    }
+    return ans;
+}
+    
     vector<int> targetIndices(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
         vector<int> res;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target)
-                res.push_back(i);
+        int lb=LB(nums, target);
+        int ub=UB(nums, target);
+        if((lb==-1)&&(ub==-1))
+            return {};
+        for(int i=lb; i<=ub; ++i){
+            res.push_back(i);
         }
         return res;
     }    
