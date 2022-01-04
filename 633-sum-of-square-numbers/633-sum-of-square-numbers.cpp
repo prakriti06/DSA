adb4279dc2ab26ef83d051456130d2b346af8522
Prakriti Sharma <prakritisharma957@gmail.com>
2022-01-04 22:34:41 +0530

    Time: 5 ms (27.23%), Space: 6 MB (26.60%) - LeetHub

--- 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-     bool Square(int a) { 
-        long int l=1, h=a, mid;
-        while(l<=h){
-            mid = l+(h-l)/2;
-            if(mid*mid == a)
-                return true;
-            else if(mid*mid>a)
-                h = mid-1;
-            else 
-                l = mid+1;
-        }
-        return false;
-     }
-    
     bool judgeSquareSum(int c) {
-        if(c==1||c==0)
-            return true;
-       for(long int i=0; i*i<=c; i++){
-          long int b = i*i;
-           if(Square(c-b))
-               return true;
-       }
+        int s = 0;
+        long int e = sqrt(c);
+        while(s <= e){
+            if(s*s + e*e == c)
+                return true;
+            else if(s*s + e*e < c)
+                s++;
+            else
+                e--;
+        }
         return false;
     }
 };
