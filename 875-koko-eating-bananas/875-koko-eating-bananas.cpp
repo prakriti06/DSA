8a816ae9a1a941ac9cec8c463fb2d07e3a2a21d5
Prakriti Sharma <prakritisharma957@gmail.com>
2022-01-20 22:02:02 +0530

    Time: 89 ms (15.00%), Space: 18.8 MB (85.48%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -4,8 +4,8 @@ public:
           int l = 1, r = 1000000000;
         while (l < r) {
             int m = (l + r) / 2, total = 0;
-            for (int p : piles)
-                total += (p + m - 1) / m;
+            for (int i=0; i<piles.size(); i++)
+                total += (piles[i] + m - 1) / m;
             if (total > h)
                 l = m + 1;
             else
