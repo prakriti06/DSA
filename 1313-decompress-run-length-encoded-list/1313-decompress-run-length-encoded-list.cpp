0864f7e8a7cc7fc749cbfe45afef60a327afdf0d
Prakriti Sharma <prakritisharma957@gmail.com>
2022-01-27 21:00:16 +0530

    Time: 8 ms (52.88%), Space: 10.1 MB (45.41%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
@@ -2,10 +2,15 @@ class Solution {
 public:
     vector<int> decompressRLElist(vector<int>& nums) {
         vector<int> res;
-        for(int i=0; i<nums.size(); i+=2){
-        res.insert(res.end(), nums[i], nums[i+1]);
-       
+        int i = 0;
+        while(2*i < nums.size()){
+            int j = nums[2*i];
+            int k = nums[2*i+1];
+            while(j--){
+                res.push_back(k);
+            }
+            i++;
         }
-         return res;
+        return res;
     }
 };
