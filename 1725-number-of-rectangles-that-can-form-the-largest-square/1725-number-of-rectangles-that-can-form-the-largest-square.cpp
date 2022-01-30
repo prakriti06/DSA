77601818295bdb722e88b57ba57d9e0b29499c8e
Prakriti Sharma <prakritisharma957@gmail.com>
2022-01-30 20:37:52 +0530

    Time: 57 ms (44.98%), Space: 20.2 MB (21.68%) - LeetHub

---
+++ 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countGoodRectangles(vector<vector<int>>& rectangles) {
+        int cnt = 0, max = 0;
+        for (vector<int>rectangle: rectangles) {
+	
+            int side = min(rectangle[0], rectangle[1]); 
+            if (side > max) { 
+                cnt = 1; 
+                max = side; 
+            }else if (side == max) { 
+                cnt++; 
+            }
+        }
+        return cnt;
+    }
+};
