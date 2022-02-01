77d507189612e92cd9621c95b141bea65ebc190e
Prakriti Sharma <prakritisharma957@gmail.com>
2022-02-01 14:20:27 +0530

    Time: 11 ms (97.75%), Space: 13.5 MB (34.75%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,38 +1,60 @@
 class Solution {
 public:
+     vector<vector<int>> res;
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();  
-        sort(nums.begin() , nums.end());  // sort the array to use the two pointers method
-        vector<vector<int>> ans;  
-        set<vector<int>> store;   // to store and remove the duplicate answers
-		
-        for(int i = 0 ; i < n; i++){
-		
-            for(int j = i + 1; j < n ; j++){
-			
-                int new_target  =  target - nums[i] - nums[j];
-                
-                int x = j+1 , y = n-1;
-                
-                while(x < y){
-				
-                    int sum = nums[x] + nums[y];
-                    
-                    if(sum > new_target) y--;
-                    else if(sum < new_target ) x++;
-                    else  {
-                        store.insert({nums[i] , nums[j] , nums[x] , nums[y]});
-                        x++;
-                        y--;
-                    };
-                }
+        sort(nums.begin(), nums.end());
+        vector<int> com;
+
+        dfs(nums, target, com, 4, 0);
+        return res;
+    }
+    
+    void dfs(vector<int>& nums, int target, vector<int>& com, int k, int level) {
+        
+        int avg = target / k;
+        if (level == nums.size() || nums[level] > avg || nums.back() < avg) 
+            return;
+        
+        if (k == 2) {
+            vector<vector<int>> curr = twosum(nums, level, target);
+            for (auto& v : curr) {
+                vector<int> com2(com.begin(), com.end());
+                com2.insert(com2.end(), v.begin(), v.end());
+                res.push_back(com2);
+            }
+            return;
+        }
+            
+        
+        for (int i = level; i < nums.size(); ++i) {
+            if (i != level && nums[i] == nums[i-1]) continue;
+            com.push_back(nums[i]);
+            dfs(nums, target - nums[i], com, k-1, i+1);
+            com.pop_back();
+        }           
+        
+    }
+    
+    vector<vector<int>> twosum(vector<int>& nums, int start, int target) {
+        vector<vector<int>> res;
+        int l  = start, r = nums.size()-1;
+        while(l < r) {
+            if (nums[l] + nums[r] == target) {
+                res.push_back({nums[l], nums[r]});
+                while(l < r && nums[l] == nums[l+1]) l++;
+                l++;
+                while(l < r && nums[r] == nums[r-1]) r--;
+                r--;
+            }
+            else if (nums[l] + nums[r] < target) {
+                while(l < r && nums[l] == nums[l+1]) l++;
+                l++;
+            }
+            else {
+                while(l < r && nums[r] == nums[r-1]) r--;
+                r--; 
             }
         }
-		
-        for(auto i : store){
-            ans.push_back(i);  // store the answers in an array(ans)
-        }
-		
-        return ans;
+        return res;
     }
 };
