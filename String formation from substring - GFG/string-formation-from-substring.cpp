bb6a048b2c865b9848ea0e57fbaaa6d676e36a14
Prakriti Sharma <prakritisharma957@gmail.com>
2022-06-13 11:50:24 +0530

    Added solution - LeetHub

---
+++ String formation from substring - GFG/string-formation-from-substring.cpp
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+		
+	int isRepeat(string s)
+	{
+	    int n=s.size();
+	    queue<char>q;
+	    q.push(s[0]);
+	    
+	    for(int i=1;i<n;i++){
+	        if(q.front()==s[i]){
+	            q.pop();
+	        }
+	        q.push(s[i]);
+	    }
+	    
+	    if(q.size()==n){
+	        return false;
+	    }
+	    
+	    for(int i=0;i<n;i++){
+	        if(q.front()!=s[i])
+	        {
+	            return false;
+	        }
+	        q.pop();
+	        q.push(s[i]);
+	        
+	    }
+	    
+	    return true;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string str;
+   		cin >> str;
+        Solution ob;
+   		cout << ob.isRepeat(str) << "\n";
+   	}
+
+    return 0;
+}  // } Driver Code Ends
