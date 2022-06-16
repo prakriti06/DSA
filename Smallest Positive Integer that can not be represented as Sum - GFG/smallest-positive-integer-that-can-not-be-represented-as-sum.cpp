dce1933a10ffc59dd5e34f5631809160b7ff87b5
Prakriti Sharma <prakritisharma957@gmail.com>
2022-06-16 23:36:45 +0530

    Added solution - LeetHub

---
+++ Smallest Positive Integer that can not be represented as Sum - GFG/smallest-positive-integer-that-can-not-be-represented-as-sum.cpp
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{   
+public:
+    long long smallestpositive(vector<long long> array, int n){ 
+        sort(array.begin(),array.end());
+        long long sum=0;
+        if(array[0]>1)return 1; 
+        for(int i=0;i<n-1;i++){
+           sum += array[i];
+           int diff = array[i+1]-array[i];
+           if(sum+1<array[i+1]){
+               return sum+1;
+           }
+       }
+       sum+=array[n-1]; 
+       return sum+1;
+    } 
+};
+
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<long long> array(n);
+        for (int i = 0; i < n; ++i)
+        {
+            cin>>array[i];
+        }
+        Solution ob;
+        cout<<ob.smallestpositive(array,n)<<"\n";
+    }
+    return 0; 
+} 
+  // } Driver Code Ends
