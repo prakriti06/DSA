d1e9171e78cb16a57af9ea9e60347fcc4fb28b7b
Prakriti Sharma <prakritisharma957@gmail.com>
2022-06-17 21:45:41 +0530

    Added solution - LeetHub

--- Delete without head pointer - GFG/delete-without-head-pointer.cpp
+++ Delete without head pointer - GFG/delete-without-head-pointer.cpp
@@ -80,11 +80,8 @@ class Solution
     public:
     //Function to delete a node without any reference to head pointer.
     void deleteNode(Node *del){
-        
-       Node *temp = del->next;
-       del->data = temp->data;
-       del->next = temp->next;
-       delete(temp);
+        del->data = del->next->data;
+        del->next = del->next->next;
        
     }
 };
